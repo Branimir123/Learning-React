1ae213d2c8450440555b2b42c4ed60334a8f9b16
Branimir Kirilov <branimir.kirilov@scalefocus.com>
2017-07-12 16:29:49 +0300

    Add some packages and action for fetching posts

---
+++ 4.React-Router/src/components/posts-index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class PostsIndex extends Component {
+    render(){
+        return(
+            <div>
+                Posts Index Page
+            </div>
+        );
+    }
+}
