a578b28f8b1c3594ba2f25c351b3aeeeaef43de0
Branimir Kirilov <branimir.kirilov@scalefocus.com>
2017-07-12 14:06:14 +0300

    Add more styling

--- 3.WeatherForecast/src/components/app.js
+++ 3.WeatherForecast/src/components/app.js
@@ -7,7 +7,7 @@ import WeatherList from '../containers/weather-list'
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app-component">
         <SearchBar />
         <WeatherList />
       </div>
