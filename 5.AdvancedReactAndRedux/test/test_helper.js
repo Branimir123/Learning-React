e13f0ea5f5e21d63d367e61f4b247ba2a60cfd48
Branimir Kirilov <branimir.kirilov@scalefocus.com>
2017-07-13 17:35:57 +0300

    Set up running like browser in command line, build renderComponent helper function

--- 5.AdvancedReactAndRedux/test/test_helper.js
+++ 5.AdvancedReactAndRedux/test/test_helper.js
@@ -1,7 +1,18 @@
-// Set up testing enviroment to run like a browser in the command line 
+import jsdom from 'jsdom';
+import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
 
+// Set up testing enviroment to run like a browser in the command line 
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+const $ = _$(global.window);
 
 // Build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass) {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
+}
 
 
 // Build helper for simulating events
