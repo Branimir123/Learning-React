9831082bd5c0452532958a8630e2db4b86e5dfd5
Branimir Kirilov <branimir.kirilov@scalefocus.com>
2017-07-13 16:03:22 +0300

    Add tests for CommentList Component

--- 5.AdvancedReactAndRedux/src/components/comment-list.js
+++ 5.AdvancedReactAndRedux/src/components/comment-list.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Component } from 'react';
+import { connect } from 'react-redux';
 
 const CommentList = (props) => {
+    const list = props.comments.map(
+        comment => 
+            <li key={comment}>{comment}</li>
+    );
+
     return(
-         <div>
-            <ul className="comment-list"></ul>
-         </div>
+       <ul className="comment-list"></ul>
     );
 }
 
-export default CommentList;
+function mapStateToProps(state){
+    return { comments: state.comments };
+}
+
+export default connect(mapStateToProps)(CommentList);
--- 5.AdvancedReactAndRedux/test/components/comment-list-tests.js
+++ 5.AdvancedReactAndRedux/test/components/comment-list-tests.js
@@ -3,24 +3,20 @@ import CommentList from '../../src/components/comment-list';
 
  describe('CommentList Component', () =>{
     let component;
-    let value = "Some dummy value";
 
     beforeEach(() => {
-        component = renderComponent(CommentList, null, { comments: [{value}, {value}] });
+        const props = { comments: ['comment1', 'comment2'] };
+        component = renderComponent(CommentList, null, props);
     });
   
     describe('sub-elements existance', () => {
-        it('has an unordered list element', () => {
-            expect(component.find('ul')).to.exist;
-        });
-
-        it('shows an li for each comment', () => {
-            expect(component.find('li').length).to.equals(2);
+        it('shows a li for each comment', () => {
+            expect(component.find('li').length).to.equal(2);
         });
 
         it('shows each comment that is provided', () => {
-            expect(component).to.contain(value);
-            expect(component).to.contain(value);    
+            expect(component).to.contain('comment1');
+            expect(component).to.contain('comment2');    
          });
     });  
  });
