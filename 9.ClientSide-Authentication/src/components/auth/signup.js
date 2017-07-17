9e843d2aa4afd46db4ccabee574e4516eb18e3a3
Branimir123 <brani_svetoslavov@abv.bg>
2017-07-17 17:00:43 +0300

    add fields and labels in signup component

--- 9.ClientSide-Authentication/src/components/auth/signup.js
+++ 9.ClientSide-Authentication/src/components/auth/signup.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
+import { connect } from 'react-redux';
 
 class Signup extends Component {
     render () {
+        const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
+
         return (
-            <div>Signup</div>
+            <form>
+                <fieldset className="form-group">
+                    <label>Email:</label>
+                    <input type="text" className="form-control" {...email}/>
+                </fieldset>   
+                <fieldset className="form-group">
+                    <label>Password:</label>
+                    <input type="text" className="form-control" {...password} type="password"/>
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Confirm Password:</label>
+                    <input type="text" className="form-control" {...passwordConfirm} type="password"/>
+                </fieldset>
+                <button action="submit" className="btn btn-primary">Sign up</button>
+            </form>
         );
     }
 }
 
-export default Signup;
+Signup = reduxForm({
+    form: 'signup',
+    fields: ['email', 'password', 'passwordConfirm']
+})(Signup);
+
+export default Signup = connect(null, null)(Signup);
