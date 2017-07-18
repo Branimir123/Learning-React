2a0bb700f9644e7b8b17ae4db16b3e17c6895767
Branimir123 <brani_svetoslavov@abv.bg>
2017-07-18 15:09:45 +0300

    Finish requests to private server routes. Finish second course

--- 8.FinalMiniProject-Authentication/Client-Authentication/src/actions/index.js
+++ 8.FinalMiniProject-Authentication/Client-Authentication/src/actions/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
+import { 
+    AUTH_USER, 
+    UNAUTH_USER, 
+    AUTH_ERROR, 
+    FETCH_MESSAGE
+} from './types';
 
 const ROOT_URL = 'http://localhost:3000';
 
@@ -58,5 +63,19 @@ export function authError(error) {
     return {
         type: AUTH_ERROR,
         payload: error
+    }
+}
+
+export function fetchMessage() {
+    return function(dispatch) {
+        axios.get(ROOT_URL, {
+            headers: { authorization: localStorage.getItem('token') }
+        })
+            .then((response) => {
+                dispatch({
+                    type: FETCH_MESSAGE,
+                    payload: response.data.message
+                });
+            });
     }
 }
--- 8.FinalMiniProject-Authentication/Client-Authentication/src/actions/types.js
+++ 8.FinalMiniProject-Authentication/Client-Authentication/src/actions/types.js
@@ -1,3 +1,4 @@
 export const AUTH_USER = 'AUTH_USER';
 export const NOT_AUTH_USER = 'NOT_AUTH_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
+export const FETCH_MESSAGE = 'FETCH_MESSAGE';
