589e80d43bc8fcdaeb0e9dbe0c51ac95e73c4199
Branimir123 <brani_svetoslavov@abv.bg>
2017-07-18 14:33:51 +0300

    Fix displaying of sign in sign out buttons

--- 8.FinalMiniProject-Authentication/Client-Authentication/src/components/feature.js
+++ 8.FinalMiniProject-Authentication/Client-Authentication/src/components/feature.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Component } from 'react';
 
 class Feature extends Component {
-
-
-
+    render() {
+        return(
+            <div>This is a feature!</div>
+        );
+    }
 }
 
 export default Feature;
