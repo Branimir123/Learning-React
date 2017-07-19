488869615df58f5ab6ca31857498bff5ab27e54b
Branimir123 <brani_svetoslavov@abv.bg>
2017-07-19 11:36:30 +0300

    Added geolocation component

--- Project/src/components/app.js
+++ Project/src/components/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import WeatherForecast from '../containers/weather_forecast';
+import Geolocation from '../containers/weather_forecast';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app-component">
         Do I need an umbrella
-        <WeatherForecast />
+        <Geolocation />
       </div>
     );
   }
--- Project/src/containers/weather_forecast.js
+++ Project/src/containers/weather_forecast.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchWeather } from '../actions/index';
+import { geolocated } from 'react-geolocated';
 
-class WeatherForecast extends Component {
+class Geolocation extends Component {
     render() {
         return (
-            <div>Weather data here</div>
+            !this.props.isGeolocationAvailable
+            ? <div>Your browser does not support geolocation </div>
+            : !this.props.isGeolocationEnabled
+                ? <div> Your geolocation isn't enabled </div>
+                : this.props.coords
+                    ? <table>  
+                      <tbody>
+                        <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
+                        <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
+                        <tr><td>altitude</td><td>{this.props.coords.altitude}</td></tr>
+                        <tr><td>heading</td><td>{this.props.coords.heading}</td></tr>
+                        <tr><td>speed</td><td>{this.props.coords.speed}</td></tr>
+                        </tbody>
+                    </table>
+                    : <div>Getting the location data&hellip; </div>
         );
     }
 }
 
-export default WeatherForecast;
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false
+    },
+    userDecisionTimeout: 5000
+})(Geolocation);
